7e280aab422c89eecf84b58925c2963cf155107f
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-07-18 10:29:08 +0300

    Added Footer Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         Content
       </main>
+      <Footer />
     </Router>
   );
 }
